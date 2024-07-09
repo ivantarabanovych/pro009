2052f4bb336b9b51dbb3c32a00171d0f82f6db1d
ivantarabanovych <vanjokcitet334@gmail.com>
2024-07-10 00:08:11 +0300

    q1

---
+++ main.js
@@ -0,0 +1,11 @@
+function calculateArea(width, heigth){
+    return width * heigth;
+}
+
+let area1 = calculateArea(4,8);
+let area2 = calculateArea(5,10);
+let area3 = calculateArea(3,2)
+
+console.log("Площа першого прямокутника: " + area1);
+console.log("Площа другого прямокутника: " + area2);
+console.log("Площа третього прямокутника: " + area3);
